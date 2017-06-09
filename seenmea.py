83b2c881d758ec6a3e9e561117803279006159ab
kmartinez <km@ecs.soton.ac.uk>
2017-06-09 15:00:27 +0100

    aadded more parsing

--- seenmea.py
+++ seenmea.py
@@ -4,7 +4,7 @@
 #2    = Latitude
 #3    = N or S
 #4    = Longitude
-#5    = E or W
+#5    = E or W (negate if W)
 #6    = GPS quality indicator (0=invalid; 1=GPS fix; 2=Diff. GPS fix)
 #7    = Number of satellites in use [not those in view]
 #8    = Horizontal dilution of position
@@ -16,26 +16,49 @@
 #13   = Age in seconds since last update from diff. reference station
 #14   = Diff. reference station ID#
 #15   = Checksum
-# time date message (seen rarely>)
+# time date message (need enabling)
 # $GPZDA,hhmmss.ss,dd,mm,yyyy,xx,yy*CC
 import serial
+
+def nmealon2lon( n):
+	# convert text NMEA lon to degrees.decimalplaces
+	l = nmea[1]
+	degrees = float(l[0:3])
+	decimals = float(l[3:11])/60
+	lon = degrees + decimals
+	if nmea[2] == 'W':
+		lon = -lon
+	return(lon)
+
+def nmealat2lat( nl):
+	# convert text NMEA lat to degrees.decimalplaces
+	degrees = float(nl[0:2])
+	decimals = float(nl[2:10])/60
+	return(degrees + decimals)
+
 ser = serial.Serial('/dev/ttyACM0',9600)
 file = open("log.txt","w")
 print("lat lon alt qual hdop time sats")
 while 1:
 	data = ser.readline()
+	#data = "$GPGGA,184353.07,1929.045,S,02410.506,E,1,04,2.6,100.00,M,-33.9,M,,0000*6D"
 	
 	if data.startswith( '$GPGGA' ) or data.startswith('$GNGGA') :
 		f =  data.split(',')			
 		gpstime = f[1].split(".")[0]
 		lat = f[2]
 		lon = f[4]
+		E = f[5]
 		qual = f[6]
 		sats = f[7]		
 		hdop = f[8]		
 		alt = f[9]
-		nmea = (lat, lon, alt, qual, hdop, gpstime, sats)
+		nmea = (lat, lon, E, alt, qual, hdop, gpstime, sats)
 		print format(nmea)
-		file.write(format(nmea) + "\n")
+		print( nmealat2lat(lat) )
+		print( nmealon2lon(nmea) )
+		#file.write(format(nmea) + "\n")
 			
-			
+	if data.startswith( '$GPZDA' ) or data.startswith('$GNZDA') :
+		print data
+				
