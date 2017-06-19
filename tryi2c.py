4aabe62d5d9b9f4e87ec7358fbfb881cfe2b1690
kmartinez <km@ecs.soton.ac.uk>
2017-06-19 18:36:33 +0100

    set to debug RTC registers

--- tryi2c.py
+++ tryi2c.py
@@ -6,14 +6,18 @@ import utime
 from machine import Pin
 from pyb import I2C
 
-data = bytearray(8)
+data = bytearray(16)
 # 
 # use bus 2
 i2c = I2C(2, I2C.MASTER)
-i2c.init(I2C.MASTER, baudrate=20000) 
+i2c.init(I2C.MASTER, baudrate=40000)
 
 # i2c.deinit() # turn off peripheral
 
-# read 1 bytes from deviceID 42 starting from addr 0 
-print(i2c.mem_read(1, 0x102, 0))
+# read 10 bytes from deviceID 104 (our DS3231) starting from addr 0 
+for n in range(0,10):
+	b = i2c.mem_read(1, 104, n)
+	print(b)
+print(i2c.mem_read(1, 104, 0xe))
+print(i2c.mem_read(1, 104, 0xf))
 
