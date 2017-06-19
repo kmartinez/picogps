a7e1e8ef3d78bf412550ad60750efd2b00e51de9
kmartinez <km@ecs.soton.ac.uk>
2017-06-19 12:09:49 +0100

    stripped down version

---
+++ ds3231.py
@@ -0,0 +1,94 @@
+# Pyboard driver for DS3231 precison real time clock.
+# Adapted from WiPy driver at https://github.com/scudderfish/uDS3231
+# Includes routine to calibrate the Pyboard's RTC from the DS3231
+# delta method now operates to 1mS precision
+# precison of calibration further improved by timing Pyboard RTC transition
+# Adapted by Peter Hinch, Jan 2016
+# stripped down to i2c bus 2 by Kirk
+
+import utime, pyb
+DS3231_I2C_ADDR = 104
+
+class DS3231Exception(OSError):
+    pass
+
+rtc = pyb.RTC()
+
+def now():  # Return the current time from the RTC in millisecs from year 2000
+    secs = utime.time()
+    ms = 1000 * (255 -rtc.datetime()[7]) >> 8
+    if ms < 50:                                 # Might have just rolled over
+        secs = utime.time()
+    return 1000 * secs + ms
+
+def nownr():  # Return the current time from the RTC: caller ensures transition has occurred
+     return 1000 * utime.time() + (1000 * (255 -rtc.datetime()[7]) >> 8)
+
+# Driver for DS3231 accurate RTC module (+- 1 min/yr) needs adapting for Pyboard
+# source https://github.com/scudderfish/uDS3231
+def bcd2dec(bcd):
+    return (((bcd & 0xf0) >> 4) * 10 + (bcd & 0x0f))
+
+def dec2bcd(dec):
+    tens, units = divmod(dec, 10)
+    return (tens << 4) + units
+
+class DS3231:
+    def __init__(self):
+        self.ds3231 = pyb.I2C(2, mode=pyb.I2C.MASTER, baudrate=400000)
+        self.timebuf = bytearray(7)
+        if DS3231_I2C_ADDR not in self.ds3231.scan():
+            raise DS3231Exception("DS3231 not found on I2C bus at %d" % DS3231_I2C_ADDR)
+
+    def get_time(self, set_rtc = False):
+        if set_rtc:
+            data = self.await_transition()      # For accuracy set RTC immediately after a seconds transition
+        else:
+            data = self.ds3231.mem_read(self.timebuf, DS3231_I2C_ADDR, 0) # don't wait
+        ss = bcd2dec(data[0])
+        mm = bcd2dec(data[1])
+        if data[2] & 0x40:
+            hh = bcd2dec(data[2] & 0x1f)
+            if data[2] & 0x20:
+                hh += 12
+        else:
+            hh = bcd2dec(data[2])
+        wday = data[3]
+        DD = bcd2dec(data[4])
+        MM = bcd2dec(data[5] & 0x1f)
+        YY = bcd2dec(data[6])
+        if data[5] & 0x80:
+            YY += 2000
+        else:
+            YY += 1900
+        if set_rtc:
+            rtc.datetime((YY, MM, DD, wday, hh, mm, ss, 0))
+        return (YY, MM, DD, hh, mm, ss, wday -1, 0) # Time from DS3231 in time.time() format (less yday)
+
+    def save_time(self):
+        (YY, MM, DD, wday, hh, mm, ss, subsecs) = rtc.datetime()
+        self.ds3231.mem_write(dec2bcd(ss), DS3231_I2C_ADDR, 0)
+        self.ds3231.mem_write(dec2bcd(mm), DS3231_I2C_ADDR, 1)
+        self.ds3231.mem_write(dec2bcd(hh), DS3231_I2C_ADDR, 2)      # Sets to 24hr mode
+        self.ds3231.mem_write(dec2bcd(wday), DS3231_I2C_ADDR, 3)    # 1 == Monday, 7 == Sunday
+        self.ds3231.mem_write(dec2bcd(DD), DS3231_I2C_ADDR, 4)
+        if YY >= 2000:
+            self.ds3231.mem_write(dec2bcd(MM) | 0b10000000, DS3231_I2C_ADDR, 5)
+            self.ds3231.mem_write(dec2bcd(YY-2000), DS3231_I2C_ADDR, 6)
+        else:
+            self.ds3231.mem_write(dec2bcd(MM), DS3231_I2C_ADDR, 5)
+            self.ds3231.mem_write(dec2bcd(YY-1900), DS3231_I2C_ADDR, 6)
+
+    def delta(self):                            # Return no. of mS RTC leads DS3231
+        self.await_transition()
+        rtc_ms = now()
+        t_ds3231 = utime.mktime(self.get_time())  # To second precision, still in same sec as transition
+        return rtc_ms - 1000 * t_ds3231
+
+    def await_transition(self):                 # Wait until DS3231 seconds value changes
+        data = self.ds3231.mem_read(self.timebuf, DS3231_I2C_ADDR, 0)
+        ss = data[0]
+        while ss == data[0]:
+            data = self.ds3231.mem_read(self.timebuf, DS3231_I2C_ADDR, 0)
+        return data
+
