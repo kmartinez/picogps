c811c07c75eb2e3e7011799e909b09ac8403f504
kmartinez <km@ecs.soton.ac.uk>
2017-06-19 12:09:17 +0100

    uses stripped ds3231 now

--- setRTC.py
+++ setRTC.py
@@ -1,13 +1,13 @@
-from ds3231_pb import DS3231
+from ds3231 import DS3231
 
 import utime, pyb
 
 rtc = pyb.RTC()
 
-extrtc = DS3231("y")
+extrtc = DS3231()
 
-def now():
+def printnow():
 	print("ext rtc:")
 	print ( extrtc.get_time() )
 
-
+printnow()
