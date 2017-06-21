1d031c291c08d04c4e8a18191fddee1d3137298b
kmartinez <km@ecs.soton.ac.uk>
2017-06-21 15:48:27 +0100

    fixed initialisations

--- boot.py
+++ boot.py
@@ -7,8 +7,8 @@ import pyb
 #pyb.usb_mode('VCP+MSC') # act as a serial and a storage device
 #pyb.usb_mode('VCP+HID') # act as a serial device and a mouse
 
-GPSpower = pyb.Pin('B13',Pin.OUT)
-Satpower = pyb.Pin('B14',Pin.OUT)
-gpsuart = UART(1,38400)
-satuart = UART(2,19200)
+GPSpower = pyb.Pin('B13',machine.Pin.OUT)
+Satpower = pyb.Pin('B14',machine.Pin.OUT)
+gpsuart = pyb.UART(1,38400)
+satuart = pyb.UART(2,19200)
 
