614e21c362f8ae6f234f028c1e1ec3d28a5621ce
Josh Curry <josh.curry@me.com>
2017-07-21 13:51:20 +0100

    Current state commit

--- mainmain.py
+++ mainmain.py
@@ -6,44 +6,83 @@ from gps import *
 from sat import *
 from common import d
 
-#Setup debugging
-
-disableDebug()
-
-#Disable actually sending iridium messages (because $$$)
-dryrun = 1
-
-# just been woken up by alarm
-# check we are on a scheduled hour
-schedule = (0, 3, 6, 9, 12, 15, 18, 21)
+schedule = [0 3 6 9 11 12 15 18 21]
+transmit = [11]
 
 #Get the current time
 extrtc = DS3231()
 (YY, MM, DD, hh, mm, ss, wday n1, n2) = extrtc.get_time()
 
-if(hh in schedule and mm<10):
-	#We're on schedule. Run the code as normal.
-	main()
+if(hh in schedule and mm<10 and hh in transmit):
+	#Satellite transmission time.
+	satloop()
+
+elif (hh in schedule and mm<10 and hh not in transmit):
+	#GPS reading time
+	gpsloop()
 else:
-	# if not we're in maintenance mode exit()
-	enableDebug()
-	d("Woken not on-schedule. Debug mode activated.")
-	exit()
+	#We've been woken up externally. Wait for CTRL-C or sleep.
+	sleep(10)
 
 
-#Setup Globals
 
-finalgps = ''
+def satloop():
 
-def main():
-	d("We're on schedule. ")
+	data = []
+	payload = ""
 
-	# turn on GPS
+
+	with open('data.txt','r') as file:
+		i = 0
+		for line in file:
+			#Check we are only sending a few readings.
+			i = i + 1
+			if(i>8):
+				break
+			#Load readings into file
+			line_Str=file.readline()
+			line_Str=line_Str.rstrip('\n')
+			line_Str=line_Str.rstrip('\r')
+			data.append(line)
+
+		
+	for item in data:
+		payload = payload + data
+
+	# turn SAT on
+	SATpower.value(1)
+	#wait for sat to boot
+	waitforsat(satuart)
+
+	strength = satsignal(satuart)
+		if strength != None:
+			d('Sat strength: ' + strength)
+		else:
+			pass
+
+	# send fix via sat
+
+	d('Sending fix via satellite')
+	sendmsg(satuart, ''.join(finalgps))
+	d('Done interfacing with sat.')
+
+	#turn SAT off
+	SATpower.value(0)
+	#we're done.
+	return
+
+
+
+def gpsloop():
+
+	#turn GPS on
 	GPSpower.value(1)
 
-	# wait for GPS fix OR timeout - store
+	t = 0
+	gps10 = []
 
-	while t<timeout:
+	while t<positiontimeout:
+		start = millis()
 		nmea = gpsuart.readline()
 		thetype, data = processGPS(nmea)
 
@@ -64,65 +103,26 @@ def main():
 
 			(lat,lon,alt,qual,hdop,sats) = data
 
-			if(qual>=5):
+			if(qual>=4):
 				#We are happy with the quality of the GPS fix.
 				finalgps = data
-				break
+				gps10.append([lat,lon])
 
 		else:
 			#Theres been some kind of problem. Timeout?
 			d('No data received - Timeout?')
 			pass
 
+
+		#Only store last 10 gps readings in array
+		if(gps10.length>10):
+			gps10.pop(0)
+
+		foreach()
+
+
+		#Set the time for our positiontimeout
+		t = millis()-start
+
 	# turn GPS off
 	GPSpower.value(0)
-
-	# turn SAT on
-	SATpower.value(1)
-
-	#wait for sat to boot
-	waitforsat(satuart)
-
-	strength = satsignal(satuart)
-		if strength != None:
-			d('Sat strength: ' + strength)
-
-	# send fix via sat
-
-	if(!dryrun) {
-		d('Sending fix via satellite')
-		sendmsg(satuart, ''.join(finalgps))
-		d('Done interfacing with sat.')
-	}
-
-	SATpower.value(0)
-	
-	# turn SAT off
-
-	# if not successful 
-	#  store GPS fix in the file (check file is not at maxsize)
-
-	# set next wakeup from schedule
-
-	nexthour = -1
-
-	for item in schedule:
-		if(item>hh):
-			nexthour = item
-			break
-	#We didnt find any next hour (roll around to beginning)
-	if(nexthour==-1):
-		nexthour = schedule[0]
-
-	extrtc.setalarm(nexthour)
-
-	d('END OF CODE')
-	exit()
-
-	#Need to be careful. If you wake it up and want it to go 
-	#back into normal operation, need a function to set the RTC 
-	#to the next available time and run again.
-
-	# if we are not on USB:
-	# deepsleep
-	# pyb.stop()
