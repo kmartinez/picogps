71ee5451ae9624e2183711f159e3fbdf4d5e9ad0
kirk <km@ecs.soton.ac.uk>
2017-08-06 17:35:04 +0100

    more debug

--- sat.py
+++ sat.py
@@ -1,41 +1,28 @@
 #!/usr/bin/env python
-# try Rockblock sat coms on UART 1 (tiny pins)
-# for normal systems: import serial
+# use Rockblock sat coms on UART 1 (tiny pins)
 # may have to disable handshake with AT&K0 ???
-# may have to read back the stuff sent as its echod
-#from pyb import UART
-#import serial
-#import utime
-#ser = UART(2,19200)
-
-# normal systems:
+# have to read back anything sent as its echod
 from time import sleep
 import pyb
-# ser = serial.Serial('/dev/ttyUSB0',19200)
-# can set the timeout option too in ms:
-# ser.init(19200,nits=8,parity=None,timeout=50)
-
 # gives 0 none to 5 strong
 # after few s it replies +CSQ:2 newline then OK
-
-ser = pyb.UART(2,19200)
-
+satuart = pyb.UART(2,19200)
 # sig str can take > 4s
 def satsignal():
 	print('getting signal strength')
 	msg = 'AT+CSQ\r'
-	ser.write(msg)
+	satuart.write(msg)
 	# give it time so we dont timeout
 	#utime.sleep_ms(100)
 	sleep(4)
 	#discard our echo
-	ser.readline()
-	ret = ser.readline()
+	satuart.readline()
+	ret = satuart.readline()
 	#blank line
-	ser.readline()
+	satuart.readline()
 	#OK
 	ret = str(ret)
-	ser.readline()
+	satuart.readline()
 	if ret != None:
 		n =  ret.find('+CSQ')
 		if n != -1:
@@ -47,7 +34,7 @@ def satsignal():
 def waitforOK():
 	count = 10
 	while count > 0 :
-		ret = ser.read()
+		ret = satuart.read()
 		if ret == None:
 			count = count - 1
 		else:
@@ -59,37 +46,38 @@ def waitforsat():
 	count = 10
 	while count > 0:
 		print('sending AT')
-		ser.write('AT\r')
+		satuart.write('AT\r')
 		#discard our echo
-		ser.readline()
+		satuart.readline()
 		#utime.sleep_ms(200)
 		sleep(1)
-		ret = ser.readline()
+		ret = satuart.readline()
 		if ret != None:
 			print(ret)
 			count = 0
 		else:
+			count = count -1
 			print('nothing yet')
 
 def sendtext(msg): 
 	print('sending message')
 	txt = msg + '\r'
-	ser.write(txt)
+	satuart.write(txt)
 	#waitforOK()
-	print(ser.read())
+	print(satuart.read())
 
 # send a msg - can take many seconds
 def sendmsg(msg): 
 	print('sending message')
 	txt = 'AT+SBDWT=' + msg + '\r'
-	ser.write(txt)
+	satuart.write(txt)
 	waitforOK()
 	# was it ok?
-	ser.write('AT+SBDIX\r')
+	satuart.write('AT+SBDIX\r')
 	#discard our echo
-	ser.readline()
+	satuart.readline()
 	# probably need sleep 1 or 2
-	ret = ser.read()
+	ret = satuart.read()
 	# SUCCESS is +SBDIX: 0, 0, 0, 0, 0, 0
 	# FAIL like +SBDIX: 32, 1, 2, 0, 0, 0
 	print(ret)
@@ -98,8 +86,3 @@ def sendmsg(msg):
 		d("msg sent")
 
 
-# waitforsat(ser)
-
-# strength = satsignal()
-# if strength != None:
-# 	print strength
