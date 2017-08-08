e873281ca3f007d462b2d3d0d1866b3fc4adba55
kmartinez <km@ecs.soton.ac.uk>
2017-08-08 16:47:22 +0100

    simpler sig strength parser

--- sat.py
+++ sat.py
@@ -4,40 +4,52 @@
 # have to read back anything sent as its echod
 from time import sleep
 import pyb
+from utime import sleep_ms
+from common import d
 # gives 0 none to 5 strong
 # after few s it replies +CSQ:2 newline then OK
+# We NEED TO INHERIT THIS FROM BOOT?
 satuart = pyb.UART(2,19200)
+satuart.init(19200,bits=8,parity=None,timeout=60)
 # sig str can take > 4s
+# VERY BUGGY AT THE MOMENT!
 def satsignal():
 	print('getting signal strength')
 	msg = 'AT+CSQ\r'
 	satuart.write(msg)
-	# give it time so we dont timeout
-	#utime.sleep_ms(100)
-	sleep(4)
 	#discard our echo
-	satuart.readline()
-	ret = satuart.readline()
-	#blank line
-	satuart.readline()
+	#satuart.readline()
+	#discard blank line
+	#ret = satuart.readline()
+	#d(ret)
+	#sleep(1)
+	#ret = satuart.readline()
+	#d(ret)
 	#OK
-	ret = str(ret)
-	satuart.readline()
-	if ret != None:
-		n =  ret.find('+CSQ')
-		if n != -1:
-			#print(ret.strip())
-			return( int(ret[n+5:n+5+1]) )
-	else:
-		return(None)
+	#ret = satuart.readline()
+	#d(ret)
+	# now we hope for reply
+	# at some read we will get CSQ:3\r\n
+	count = 60
+	while count > 0:
+		ret = str(satuart.readline())
+		d(ret)
+		csqpos = ret.find("CSQ:")
+		if (csqpos >= 0) and (len(ret) >5) :
+			return ( ret[4 + csqpos] )
+		count = count -1
+		sleep_ms(100)
+	return(0)
 
 def waitforOK():
-	count = 10
+	count = 20
 	while count > 0 :
 		ret = satuart.read()
 		if ret == None:
 			count = count - 1
+			sleep_ms(50)
 		else:
+			d('got OK')
 			return(True)
 	return(False)
 		
@@ -53,7 +65,7 @@ def waitforsat():
 		sleep(1)
 		ret = satuart.readline()
 		if ret != None:
-			print(ret)
+			d(ret)
 			count = 0
 		else:
 			count = count -1
@@ -72,17 +84,24 @@ def sendmsg(msg):
 	txt = 'AT+SBDWT=' + msg + '\r'
 	satuart.write(txt)
 	waitforOK()
+	sleep(1)
 	# was it ok?
 	satuart.write('AT+SBDIX\r')
 	#discard our echo
 	satuart.readline()
 	# probably need sleep 1 or 2
-	ret = satuart.read()
-	# SUCCESS is +SBDIX: 0, 0, 0, 0, 0, 0
-	# FAIL like +SBDIX: 32, 1, 2, 0, 0, 0
-	print(ret)
-	status = ret.split(",")[0].split(" ")[1]
-	if status == "0":
-		d("msg sent")
-
+	sleep(1)
+	count = 10
+	while count > 0 :
+		ret = satuart.read()
+		# SUCCESS is +SBDIX: 0, 0, 0, 0, 0, 0
+		# FAIL like +SBDIX: 32, 1, 2, 0, 0, 0
+		print(ret)
+		if ret != None:
+			status = ret.split(",")[0].split(" ")[1]
+			if status == "0":
+				d("msg sent")
+			return(True)
+		count = count - 1
+		sleep_ms(100)
 
