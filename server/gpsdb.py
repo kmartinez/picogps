afbf584c97d6ece6661e6dd3ddee20eac307fe71
Philip Basford <pjb@ecs.soton.ac.uk>
2017-08-15 21:28:09 +0100

    First version of database interface

---
+++ server/gpsdb.py
@@ -0,0 +1,77 @@
+from configobj import ConfigObj
+import MySQLdb
+import logging
+
+class GpsDb(object):
+
+    def __init__(self, config_file, logging_level=logging.ERROR):
+        self.logger = logging.getLogger("GpsDB")
+        self.logger.setLevel(logging_level)
+        self.logger.debug("Loading database config")
+        self.db_config = GpsDbConfig(config_file)
+        self.db = None
+        self.connect()
+
+    def connect(self):
+        host = self.db_config.server
+        user = self.db_config.user
+        password = self.db_config.password
+        database = self.db_config.database
+        try:
+            db = MySQLdb.connect(
+                host=host, user=user,
+                passwd=password, db=database)
+            self.logger.info("Connected to database %s on %s", database, host)
+            self.db = db
+        except MySQLdb.Error, e:
+            self.logger.critical(
+                "Unable to connect to db %s on %s as user %s",
+                database, host, user)
+            self.logger.critical(e)
+            raise GpsDbError(str(e))
+
+    def connected(self):
+        return self.db is not None
+
+    def get_unprocessed_messages(self):
+        if not self.connected():
+            raise GpsDbError()
+        self.db.query("SELECT COUNT(*) FROM iridium_raw WHERE 'processed' = 0")
+        result = self.db.store_result()
+        return result.fetch_row(0)[0][0]
+        
+    def save_position(self, imei, timestamp, lat, lon, alt, qual, hdop, sats):
+        if not self.connected():
+            raise GpsDbError()
+        cursor = self.db.cursor()
+        cursor.execute(
+            "INSERT IGNORE INTO tracker_data (imei, timestamp, longitude, latitude, altitude, quality, hdop, sats) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+            (imei, str(timestamp), lon, lat, alt, qual, hdop, sats))
+        cursor.close()
+        print("INSERT IGNORE INTO tracker_data (imei, timestamp, longitude, latitude, altitude, quality, hdop, sats) VALUES (%s, \"%s\", %s, %s, %s, %s, %s, %s)",
+            (imei, str(timestamp), lon, lat, alt, qual, hdop, sats))
+        cursor.close()
+        self.db.commit()
+        self.logger.debug("Position saved")
+
+
+class GpsDbConfig(object):
+    def __init__(self, config_file):
+        try:
+            config = ConfigObj(config_file)
+            self.database = config["database"]
+            self.server = config["server"]
+            self.user = config["user"]
+            self.password = config["pass"]
+        except KeyError:
+            raise ConfigError("Invalid config File")
+
+
+class ConfigError(Exception):
+    """
+        An error for when something has gone wrong reading the config
+    """
+    pass
+
+class GpsDbError(Exception):
+    pass
--- server/parsenmea.py
+++ server/parsenmea.py
@@ -9,11 +9,13 @@
 #8    = Number of satellites in use [not those in view]
 # this sample should be at 50.953711210 1.641447010
 #sample = '1707301731,5057.2226726,138.4868206,W,130.71,5,1.4,8'
-sample = '1707301731,6354.1234567,1638.1234567,W,130.71,5,1.4,8'
+sample = '1707301731,6354.1234567,1638.1234567,130.71,5,1.4,8'
+import datetime
+
 
 def parsefixdata(data):
 	f =  data.split(',')			
-	timestamp = f[0]
+	timestamp = datetime.datetime.strptime(f[0], "%y%m%d%H%M")
 	# only works if above 10 degrees!
 	lat = float(f[1][0:2]) + (float(f[1][2:])/60.0)
 	# carefully take one or two figures from lat as we stripped 00s
@@ -28,10 +30,10 @@ def parsefixdata(data):
 	#E = f[3]
 	#assume all West if f[3] == 'W':
 	lon = -lon
-	alt = f[3]
-	qual = f[4]
-	hdop = f[5]		
-	sats = f[6]
+	alt = float(f[3])
+	qual = int(f[4])
+	hdop = float(f[5])
+	sats = int(f[6])
 	location = (timestamp,lat,lon, alt, qual,hdop,sats)
 	return location
 
